ef7cb380a22d5cef4c8c73c0e7080f5947a23181
Oliverso2K <oliverisrael.quinterogarcia@gmail.com>
2022-11-15 14:18:45 -0600

    update: Index
    Diseño del index, implementando una plantilla CSS.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,71 +2,142 @@
 <html lang="en">
 
 <head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content="" />
-    <meta name="author" content="" />
+    <meta charset="utf-8">
     <title>GreatStorage</title>
-    <!-- Favicon-->
-    <link rel="icon" type="image/x-icon" href="assets/img/logo.png" />
-    <!-- Font Awesome icons (free version)-->
-    <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    <!-- Google fonts-->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
-    <!-- Core theme CSS (includes Bootstrap)-->
-    <link href="assets/css/styles.css" rel="stylesheet" />
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="assets/img/logo.png" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@400;500;600;700&family=Rubik&display=swap" rel="stylesheet"> 
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+    <link href="lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="assets/css/style.css" rel="stylesheet">
 </head>
 
-<body id="page-top">
-    <!-- Navigation-->
-    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
-        <div class="container">
-            <a href="/index"><img src="assets/img/logo.png" width="100px"></a>
-            <a class="navbar-brand" href="/index">GreatStorage</a>
-            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                Menu
-                <i class="fas fa-bars"></i>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarResponsive">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/proveedor">Proveedores</a></li>
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/producto">Productos</a></li>
-                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded" href="/sucursal">Sucursales</a></li>
-                </ul>
+<body>
+    <!-- Header Start -->
+    <div class="container-fluid bg-dark px-0">
+        <div class="row gx-0">
+            <div class="col-lg-3 bg-dark d-none d-lg-block">
+                <a href="/index" class="navbar-brand w-100 h-100 m-0 p-0 d-flex align-items-center justify-content-center">
+                    <h1 class="m-0 display-4 text-primary text-uppercase">GreatStorage</h1>
+                </a>
+            </div>
+            <div class="col-lg-9">
+                <nav class="navbar navbar-expand-lg bg-dark navbar-dark p-3 p-lg-0 px-lg-5">
+                    <a href="/index" class="navbar-brand d-block d-lg-none">
+                        <h1 class="m-0 display-4 text-primary text-uppercase">GreatStorage</h1>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="/index" class="nav-item nav-link active">Inicio</a>
+                            <a href="/producto" class="nav-item nav-link">Productos</a>
+                            <a href="/proveedor" class="nav-item nav-link">Proveedores</a>
+                            <a href="/sucursal" class="nav-item nav-link">Sucursales</a>
+                        </div>
+                        <div class="navbar-nav mr-auto py-0">
+                            <a href="/login" class="nav-item nav-link">Inicia Sesión</a>
+                            <a href="/register" class="nav-item nav-link">Regístrate</a>
+                        </div>
+                    </div>
+                </nav>
             </div>
         </div>
-    </nav>
-    <!-- Masthead-->
-    <header class="masthead bg-primary text-white text-center" style="background-image: linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url('assets/img/index.jpg'); background-repeat: no-repeat; background-attachment: fixed; background-size: 100% 100%; filter: brightness(50%);">
-        <div class="container d-flex align-items-center flex-column">
-            <!-- Masthead Avatar Image-->
-            <!-- Masthead Heading-->
-            <h1 class="masthead-heading text-uppercase mb-0">Bienvenido a GreatStorage</h1>
-            <!-- Icon Divider-->
-            <div class="divider-custom divider-light">
-                <div class="divider-custom-line"></div>
-                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                <div class="divider-custom-line"></div>
-            </div>
-            <!-- Masthead Subheading-->
-            <p class="masthead-subheading font-weight-light mb-0">El sistema de administración de inventario que necesita tu empresa.</p>
-        </div>
-    </header>
-
-    <!-- Copyright Section-->
-    <div class="copyright py-4 text-center text-white">
-        <div class="container"><small>Copyright &copy; GreatStorage 2022</small></div>
     </div>
+    <!-- Header End -->
 
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
-    <!-- Core theme JS-->
-    <script src="assets/js/scripts.js"></script>
-    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-    <!-- * *                               SB Forms JS                               * *-->
-    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
-    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
-    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+
+    <!-- Carousel Start -->
+    <div class="container-fluid p-0 mb-5">
+        <div id="header-carousel" class="carousel slide" data-bs-ride="carousel">
+            <img class="w-100" src="assets/img/index.jpg" alt="Image">
+            <div class="carousel-caption d-flex flex-column align-items-center justify-content-center">
+                <div class="p-3" style="max-width: 900px;">
+                    <h5 class="text-white text-uppercase">El mejor sistema de administración de inventario</h5>
+                    <h1 class="display-2 text-white text-uppercase mb-md-4">Haz crecer tu negocio usando GreatStorage</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Carousel End -->
+
+
+    <!-- About Start -->
+    <div class="container-fluid p-5">
+        <div class="row gx-5">
+            <div class="col-lg-5 mb-5 mb-lg-0" style="min-height: 500px;">
+                <div class="position-relative h-100">
+                    <img class="position-absolute w-100 h-100 rounded" src="assets/img/logo.png" style="object-fit: cover;">
+                </div>
+            </div>
+            <div class="col-lg-7">
+                <div class="mb-4">
+                    <h5 class="text-primary text-uppercase">GreatStorage</h5>
+                    <h1 class="display-3 text-uppercase mb-0">Bienvenido a GreatStorage</h1>
+                </div>
+                <h4 class="text-body mb-4">Haz crecer tu negocio usando GreatStorage</h4>
+                <p class="mb-4"></p>
+                <div class="rounded bg-dark p-5">
+                    <ul class="nav nav-pills justify-content-between mb-3">
+                        <li class="nav-item w-50">
+                            <a class="nav-link text-uppercase text-center w-100 active" data-bs-toggle="pill" href="#pills-1">¿Qué es GreatStorage?</a>
+                        </li>
+                        <li class="nav-item w-50">
+                                <a class="nav-link text-uppercase text-center w-100" data-bs-toggle="pill" href="#pills-2">¿Por qué escogernos?</a>
+                        </li>
+                    </ul>
+                    <div class="tab-content">
+                        <div class="tab-pane fade show active" id="pills-1">
+                            <p class="text-secondary mb-0">GreatStorage es un sistema de administración de inventario. Nace de la idea de dos estudiantes de ingeniería informática con el objetivo de facilitar las labores de organización desde pequeñas hasta grandes empresas.</p>
+                        </div>
+                        <div class="tab-pane fade" id="pills-2">
+                            <p class="text-secondary mb-0">¿Tienes un negocio en el que necesites administrar el flujo de productos? GreatStorage te permite hacer esto a través de sus amigables herramientas, las cuales también de permiten registrar y manejar la información de proveedores y de las diversas sucursales de tu negocio.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- About End -->
+
+    <!-- Footer Start -->
+    <div class="container-fluid py-4 py-lg-0 px-5" style="background: #1a1c2b;">
+        <div class="py-lg-4 text-center">
+            <p class="text-secondary mb-0">&copy; <a class="text-light fw-bold" href="#">GreatStorage</a>. Derechos reservados.</p>
+        </div>
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
