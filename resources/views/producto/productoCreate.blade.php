8a53fa1a9a3d0abf55e240e4f2afc7aad9c3f0d1
Oliverso2K <oliverisrael.quinterogarcia@gmail.com>
2022-11-15 20:51:24 -0600

    add: CSS CRUD Producto

--- resources/views/producto/productoCreate.blade.php
+++ resources/views/producto/productoCreate.blade.php
@@ -1,33 +1,48 @@
-<x-plantilla tab="Crear Producto" titulo="Crear Producto">
-    <form action="/producto" method="post">
-        @csrf
-
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" value={{ $nombre ?? old('nombre') }}>
-        @error('nombre')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="descripcion">Descripción</label>
-        <input type="text" name="descripcion" id="descripcion" value={{ $descripcion ?? old('descripcion') }}>
-        @error('descripcion')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="precio">Precio</label>
-        <input type="number" step="0.01" name="precio" id="precio" value={{ $precio ?? old('precio') }}>
-        @error('precio')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="proveedor_id">Proveedor</label>
-        <select class="browser-default" name="proveedor_id" id="proveedor_id" required>
-            <option selected disabled>Elije una opción</option>
-            @foreach($proveedores as $proveedor)
-                <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
-            @endforeach
-        </select>
-        <br>
-        <input type="submit" value="Guardar">
-    </form>
+<x-plantilla titulo="GreatStorage - Crear Producto" tab="GreatStorage - Crear Producto">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/producto" method="POST">
+                @csrf
+                <div class="modal-header">
+                    <h4 class="modal-title">Añadir Producto</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input class="form-control" type="text" name="nombre" id="nombre" value={{ $nombre ?? old('nombre') }}>
+                        @error('nombre')
+                        <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Descripción</label>
+                        <input class="form-control" type="text" name="descripcion" id="descripcion" value={{ $descripcion ?? old('descripcion') }}>
+                        @error('descripcion')
+                            <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="precio">Precio</label>
+                        <input class="form-control" type="number" step="0.01" name="precio" id="precio" value={{ $precio ?? old('precio') }}>
+                        @error('precio')
+                            <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="proveedor_id">Proveedor</label>
+                        <select class="form-control" name="proveedor_id" id="proveedor_id" required>
+                            <option selected disabled>Elije una opción</option>
+                            @foreach($proveedores as $proveedor)
+                                <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="/producto" class="btn btn-default">Cancelar</a>
+                    <input type="submit" class="btn btn-success" value="Añadir">
+                </div>
+            </form>
+        </div>
+    </div>
 </x-plantilla>
--- resources/views/producto/productoShow.blade.php
+++ resources/views/producto/productoShow.blade.php
@@ -1,5 +1,30 @@
-<x-plantilla tab="Detalle del Producto" titulo="Detalle del Producto">
-    <a href="/producto">Listado</a>
-
-    <h2>{{ $producto->nombre }}</h2>
-</x-plantilla>
+<x-plantilla titulo="GreatStorage - Detalles Producto" tab="GreatStorage - Detalles Producto">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Detalles de Producto</h4>
+            </div>
+            <div class="modal-body">
+                <div class="form-group">
+                    <label for="nombre">Nombre</label>
+                    <p>{{ $producto->nombre }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="descripcion">Descripción</label>
+                    <p>{{ $producto->descripcion }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="precio">Precio</label>
+                    <p>{{ $producto->precio }}</p>
+                </div>
+                <div class="form-group">
+                    <label for="proveedor_id">Proveedor</label>
+                    <p>{{ $producto->proveedor->nombre }}</p>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="/producto" class="btn btn-default">Regresar</a>
+            </div>
+        </div>
+    </div>
+    </x-plantilla>
