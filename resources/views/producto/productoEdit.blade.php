3033c36680d5dd80d4d2159e0935493c689b0ee8
Miguel Mendoza <111393850+MiguelMdza@users.noreply.github.com>
2022-11-15 21:01:42 -0600

    add: CSS CRUD Producto (#8)
    Co-authored-by: Oliverso2K <oliverisrael.quinterogarcia@gmail.com>

--- resources/views/producto/productoEdit.blade.php
+++ resources/views/producto/productoEdit.blade.php
@@ -1,34 +1,50 @@
-<x-plantilla titulo="Editar Producto">
-    <form action="/producto/{{ $producto->id }}" method="post">
-        @csrf
-        @method('patch')
+<x-plantilla titulo="GreatStorage - Editar Producto" tab="GreatStorage - Editar Producto">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <form action="/producto/{{ $producto->id }}" method="POST">
+                @csrf
+                @method('PATCH')
 
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre" value="{{ old('nombre') ?? $producto->nombre }}">
-        @error('nombre')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="descripcion">Descripción</label>
-        <input type="text" name="descripcion" id="descripcion" value="{{ old('descripcion') ?? $producto->descripcion }}">
-        @error('descripcion')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="precio">Precio</label>
-        <input type="number" step="0.01" name="precio" id="precio" value="{{ old('precio') ?? $producto->precio }}">
-        @error('precio')
-            <i>{{ $message }}</i>
-        @enderror
-        <br>
-        <label for="proveedor_id">Proveedor</label>
-        <select class="browser-default" name="proveedor_id" id="proveedor_id" required>
-            <option selected disabled>Elije una opción</option>
-            @foreach($proveedores as $proveedor)
-                <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
-            @endforeach
-        </select>
-        <br>
-        <input type="submit" value="Guardar">
-    </form>
+                <div class="modal-header">
+                    <h4 class="modal-title">Editar Producto</h4>
+                </div>
+                <div class="modal-body">
+                    <div class="form-group">
+                        <label for="nombre">Nombre</label>
+                        <input class="form-control" type="text" name="nombre" id="nombre" value="{{ old('nombre') ?? $producto->nombre }}">
+                        @error('nombre')
+                        <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="descripcion">Descripción</label>
+                        <input class="form-control" type="text" name="descripcion" id="descripcion" value="{{ old('descripcion') ?? $producto->descripcion }}">
+                        @error('descripcion')
+                            <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="precio">Precio</label>
+                        <input class="form-control" type="number" step="0.01" name="precio" id="precio" value="{{ old('precio') ?? $producto->precio }}">
+                        @error('precio')
+                            <i>{{ $message }}</i>
+                        @enderror
+                    </div>
+                    <div class="form-group">
+                        <label for="proveedor_id">Proveedor</label>
+                        <select class="form-control" name="proveedor_id" id="proveedor_id" required>
+                            <option selected disabled>Elije una opción</option>
+                            @foreach($proveedores as $proveedor)
+                                <option value="{{ $proveedor->id }}">{{ $proveedor->nombre }}</option>
+                            @endforeach
+                        </select>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <a href="/producto" class="btn btn-default">Cancelar</a>
+                    <input type="submit" class="btn btn-success" value="Actualizar">
+                </div>
+            </form>
+        </div>
+    </div>
 </x-plantilla>
--- resources/views/producto/productoIndex.blade.php
+++ resources/views/producto/productoIndex.blade.php
@@ -1,38 +1,58 @@
-<x-plantilla tab="Listado de Productos" titulo="Listado de Productos">
+<x-plantilla titulo="GreatStorage - Productos" tab="GreatStorage - Productos">
+    
 
-    <a href="/producto/create">Crear Nuevo Producto</a>
-
-    <table border = "1">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Descripción</th>
-            <th>Precio</th>
-            <th>Proveedor</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-        </tr>
-        @foreach ($productos as $producto)
-            <tr>
-                <td>{{ $producto->id }}</td>
-                <td>
-                    <a href="/producto/{{ $producto->id }}">
-                    {{ $producto->nombre }}
-                </td>
-                <td>{{ $producto->descripcion }}</td>
-                <td>{{ $producto->precio }}</td>
-                <td>{{ $producto->proveedor->nombre }}</td>
-                <td>
-                    <a href="/producto/{{ $producto->id }}/edit">Editar</a>
-                </td>
-                <td>
-                    <form action="/producto/{{ $producto->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Borrar">
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container-xl">
+        <div class="table-responsive">
+            <div class="table-wrapper">
+                <div class="table-title">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h2 style="color: #fff">Lista de <b>Productos</b></h2>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="/producto/create" class="btn btn-success"><i class="material-icons">&#xE147;</i>Añadir Producto</a>
+                        </div>
+                    </div>
+                </div>
+                <table class="table table-striped table-hover">
+                    <thead style="color: black">
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Descripción</th>
+                            <th>Precio</th>
+                            <th>Proveedor</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($productos as $producto)
+                        <tr>
+                            <td>{{ $producto->id }}</td>
+                            <td>
+                                <a href="/producto/{{ $producto->id }}">
+                                    {{ $producto->nombre }}
+                                </a>
+                            </td>
+                            <td>{{ $producto->descripcion }}</td>
+                            <td>{{ $producto->precio }}</td>
+                            <td>{{ $producto->proveedor->nombre }}</td>
+                            <td>
+                                <a href="/producto/{{ $producto->id }}/edit">Editar</a>
+                            </td>
+                            <td>
+                                <form action="/producto/{{ $producto->id }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" value="Eliminar">
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </x-plantilla>
