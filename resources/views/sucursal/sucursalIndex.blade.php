d29a86776785abbf18c89ee5a18134c3e5397296
Miguel Mendoza <miguelmgangel@gmail.com>
2022-11-15 20:53:35 -0600

    Diseño CRUD Sucursal
    Listo diseño del CRUD Sucursal, falta implementar relación m:n

--- resources/views/sucursal/sucursalIndex.blade.php
+++ resources/views/sucursal/sucursalIndex.blade.php
@@ -1,38 +1,56 @@
-<x-plantilla tab="Listado de Sucursales" titulo="Listado de Sucursales">
+<x-plantilla tab="GreatStorage - Sucursales" titulo="GreatStorage - Sucursales">
 
-    <a href="/sucursal/create">Crear Nueva Sucursal</a>
-
-    <table border = "1">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Dirección</th>
-            <th>Teléfono</th>
-            <th>Encargado</th>
-            <th>Editar</th>
-            <th>Eliminar</th>
-        </tr>
-        @foreach ($sucursals as $sucursal)
-            <tr>
-                <td>{{ $sucursal->id }}</td>
-                <td>
-                    <a href="/sucursal/{{ $sucursal->id }}">
-                    {{ $sucursal->nombre }}
-                </td>
-                <td>{{ $sucursal->direccion }}</td>
-                <td>{{ $sucursal->telefono }}</td>
-                <td>{{ $sucursal->encargado }}</td>
-                <td>
-                    <a href="/sucursal/{{ $sucursal->id }}/edit">Editar</a>
-                </td>
-                <td>
-                    <form action="/sucursal/{{ $sucursal->id }}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <input type="submit" value="Borrar">
-                    </form>
-                </td>
-            </tr>
-        @endforeach
-    </table>
+    <div class="container-xl">
+        <div class="table-responsive">
+            <div class="table-wrapper">
+                <div class="table-title">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h2 style="color: #fff">Lista de <b>Sucursales</b></h2>
+                        </div>
+                        <div class="col-sm-6">
+                            <a href="/sucursal/create" class="btn btn-success"><i class="material-icons">&#xE147;</i>Añadir Sucursal</a>
+                        </div>
+                    </div>
+                </div>
+                <table class="table table-striped table-hover">
+                    <thead style="color: black">
+                        <tr>
+                            <th>ID</th>
+                            <th>Nombre</th>
+                            <th>Dirección</th>
+                            <th>Teléfono</th>
+                            <th>Encargado</th>
+                            <th>Editar</th>
+                            <th>Eliminar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($sucursals as $sucursal)
+                        <tr>
+                            <td>{{ $sucursal->id }}</td>
+                            <td>
+                                <a href="/sucursal/{{ $sucursal->id }}">
+                                    {{ $sucursal->nombre }}
+                            </td>
+                            <td>{{ $sucursal->direccion }}</td>
+                            <td>{{ $sucursal->telefono }}</td>
+                            <td>{{ $sucursal->encargado }}</td>
+                            <td>
+                                <a href="/sucursal/{{ $sucursal->id }}/edit">Editar</a>
+                            </td>
+                            <td>
+                                <form action="/sucursal/{{ $sucursal->id }}" method="post">
+                                    @csrf
+                                    @method('DELETE')
+                                    <input type="submit" value="Eliminar">
+                                </form>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </x-plantilla>
